5c7877bdbcaa8fdb9951f65322ef7ee743c6d203
tadzio <t.holtrop@planblue.com>
2024-08-31 17:21:01 +0100

    experimental try with random loss function criterion

--- src/optimizers.py
+++ src/optimizers.py
@@ -14,12 +14,14 @@ class SimulatedAnnealing:
         loss: Callable,
         initial_temp=1000,
         cooling_rate=0.95,
+        rand_criterion=False,
         stop_criterion=1e-3,
     ) -> None:
         self._max_iterations = max_iterations
         self._loss = loss
         self.current_temp = initial_temp
         self.cooling_rate = cooling_rate
+        self.rand_criterion: bool = rand_criterion
         self.stop_criterion = stop_criterion
         self._errors: list | None = None
 
@@ -30,6 +32,8 @@ class SimulatedAnnealing:
         # create list of errors at each iteration
         self._errors = []
         for i in range(self._max_iterations):
+            # TODO: implement a function that takes a step in the current neighbourhood
+            #      do not just assign random x, y coordinates
             x = np.random.randint(0, canvas.resolution[1])
             y = np.random.randint(0, canvas.resolution[0])
             canvas.draw.circle(center=(x, y), radius=5)
@@ -37,11 +41,18 @@ class SimulatedAnnealing:
             current_loss = self.loss(canvas._cache, reference)
             # calculate loss after drawing
             new_loss = self.loss(canvas.canvas, reference)
-            acceptance_criterion = np.exp((new_loss - current_loss) / self.current_temp)
-
-            if new_loss < current_loss:  # or np.random.rand() < acceptance_criterion:
+            # determine if radion acceptance criterion should be used
+            if self.rand_criterion:
+                acceptance_criterion = np.exp((current_loss - new_loss) / self.current_temp)
+                print(f"acceptance criterion: {acceptance_criterion}\n")
+            # TODO: remove if/else and set:
+            #  acceptance_criterion = np.exp((current_loss - new_loss) / self.current_temp)
+            else:
+                # in this case not
+                acceptance_criterion = 1e-9
+            # evaluate the new loss against the current loss
+            if new_loss < current_loss or np.random.rand() < acceptance_criterion:
                 # accept the new state, we don't have to revert the state of the canvas
-                # print(f"error: {new_loss}\n")
                 self._errors.append(new_loss)
             else:
                 # revert canvas to previous state
