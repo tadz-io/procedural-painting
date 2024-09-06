9cd01e318cf9a278bbedf951cfb3b00548f843a6
tadzio <t.holtrop@planblue.com>
2024-09-06 18:58:56 +0100

    experimenting w. optimizer settings

--- src/optimizers.py
+++ src/optimizers.py
@@ -13,7 +13,7 @@ class SimulatedAnnealing:
         max_iterations: int,
         loss: Callable,
         initial_temp: int = 1000,
-        cooling_rate: float = 0.95,
+        cooling_rate: float = 0.99,
         sampler: Callable[..., int] = lambda: (np.random.binomial(100, 0.5, 1) - 50)[0],
         stop_criterion: float = 1e-3,
     ) -> None:
@@ -23,10 +23,21 @@ class SimulatedAnnealing:
         self.cooling_rate = cooling_rate
         self.sampler = sampler
         self.stop_criterion = stop_criterion
-        self._errors: list | None = None
+        self._errors: list = []
+        self._entropy_norm = []
 
     def loss(self, y_hat, y):
         return self._loss(y_hat, y)
+
+    @property
+    def error_stats(self):
+        if not self._errors:
+            return 0.0, 0.0
+        return np.mean(self._errors), np.std(self._errors)
+
+    @error_stats.setter
+    def error_stats(self, loss: float) -> None:
+        self._errors.append(loss)
 
     def optimize(self, canvas: Canvas, reference):
         # create list of errors at each iteration
@@ -46,23 +57,28 @@ class SimulatedAnnealing:
             canvas.draw.circle(center=(x, y), radius=5)
             # calculate previous loss
             previous_loss = self.loss(canvas.previous, reference)
+            # update list with previous loss value
+            self.error_stats = previous_loss
+            # get standard deviation of loss
+            _, loss_std = self.error_stats
             # calculate loss after drawing
             new_loss = self.loss(canvas.canvas, reference)
-            # calculate delta in loss
-            d_e = (new_loss - previous_loss) / 1
-            self._errors.append(d_e)
+            # calculate delta in loss and normalize
+            d_e = new_loss - previous_loss  # / (loss_std + 1e-8)
             # Evaluate the new loss against the previous loss
             if new_loss > previous_loss:
                 # calculate new acceptance criteria
                 acceptance_criterion = np.exp(-(d_e) / self.current_temp)
+                self._entropy_norm.append(acceptance_criterion)
                 if np.random.rand() < acceptance_criterion:
                     # Case 1: Accept new x and y but revert the canvas
                     canvas.revert()
-                    canvas.draw.circle(center=(x, y), radius=5, color=(0, 0, 255))
+                    # canvas.draw.circle(center=(x, y), radius=5, color=(0, 0, 255))
                     # Keep x and y as they are (x_new, y_new)
                 else:
                     # Case 2: Revert both the canvas and the coordinates
                     canvas.revert()
+                    # canvas.draw.circle(center=(x, y), radius=5, color=(255, 0, 0))
                     # Revert x and y to the values before the random step
                     x, y = x_old, y_old
             # cool down temperature
