dd5b75fe4872c59c07c92cd01ac8c3a0fea8c20a
tadzio <t.holtrop@planblue.com>
2024-09-10 19:43:21 +0100

    create benchmarking module

---
+++ benchmark/bench_optimization.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+
+from draw import Canvas
+from optimizers import SimulatedAnnealing, mse
+
+# maximum number of iterations
+N_INTERATIONS = 5000
+
+ref_image = cv.imread("./benchmark/img/example_001.png")
+
+canvas = Canvas(cache_size=N_INTERATIONS)
+sa_optimizer = SimulatedAnnealing(
+    max_iterations=N_INTERATIONS,
+    loss=mse,
+)
+
+# do the optimization
+sa_optimizer.optimize(canvas=canvas, reference=ref_image)
