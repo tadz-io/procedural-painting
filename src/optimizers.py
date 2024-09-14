89f63f83204a57fdb8018e096eec357296be277b
tadzio <t.holtrop@planblue.com>
2024-09-14 20:34:26 +0100

    add Parameter class and refactor SimulatedAnnealing class

--- src/optimizers.py
+++ src/optimizers.py
@@ -1,12 +1,68 @@
 import numpy as np
+from numpy.typing import NDArray
 from typing import Callable
 from draw import Canvas
+from typing import Dict, Tuple
 
 
-def mse(x, y):
+def mse(x: NDArray, y: NDArray):
+    # need to convert to 16 bit uints when taking square of 8 bit arrays
     x_uint16 = x.astype(np.uint16)
     y_uint16 = y.astype(np.uint16)
     return np.mean((x_uint16 - y_uint16) ** 2)
+
+
+class Parameter:
+    def __init__(
+        self,
+        initial: int | Tuple[int, ...],
+        step_size: int | Tuple[int, ...],
+        min_value: int | Tuple[int, ...],
+        max_value: int | Tuple[int, ...],
+    ) -> None:
+        """
+        Parameter class to hold the initial value and step size for optimization.
+
+        Args:
+            initial: Initial value of the parameter.
+            step_size: Step size for the parameter. If 0, parameter doesn't change.
+        """
+        self.value = initial
+        self.step_size = step_size
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def update(self, sampler: Callable[[int, int], int]) -> None:
+        """
+        Generate a random step within the step_size range using the sampler method.
+
+        Args:
+            sampler: The callable to generate random values.
+        """
+        # If the value is an int, handle as a single parameter
+        if isinstance(self.value, int):
+            if self.step_size != 0:
+                step = sampler(-self.step_size, self.step_size)
+                self.value = np.clip(self.value + step, self.min_value, self.max_value)
+
+        # If the value is a tuple, handle as multiple parameters
+        elif isinstance(self.value, tuple):
+            # If step_size is an int, apply the same step to all elements in the tuple
+            if isinstance(self.step_size, int):
+                step = sampler(-self.step_size, self.step_size)
+                self.value = tuple(
+                    np.clip(v + step, min_v, max_v)
+                    for v, min_v, max_v in zip(self.value, self.min_value, self.max_value)
+                )
+
+            # If step_size is a tuple, generate a step for each element
+            elif isinstance(self.step_size, tuple):
+                self.value = tuple(
+                    np.clip(v + sampler(-s, s), min_v, max_v)
+                    for v, s, min_v, max_v in zip(
+                        self.value, self.step_size, self.min_value, self.max_value
+                    )
+                )
 
 
 class SimulatedAnnealing:
@@ -16,7 +72,7 @@ class SimulatedAnnealing:
         loss: Callable,
         initial_temp: int = 1000,
         cooling_rate: float = 0.999,
-        sampler: Callable[..., int] = lambda: (np.random.binomial(100, 0.5, 1) - 50)[0],
+        sampler: Callable[[int, int], int] = np.random.randint,
         stop_criterion: float = 1e-3,
     ) -> None:
         self._max_iterations = max_iterations
@@ -26,7 +82,6 @@ class SimulatedAnnealing:
         self.sampler = sampler
         self.stop_criterion = stop_criterion
         self._errors: list = []
-        self._entropy_norm = []
 
     def loss(self, y_hat, y):
         return self._loss(y_hat, y)
@@ -41,65 +96,48 @@ class SimulatedAnnealing:
     def error_stats(self, loss: float) -> None:
         self._errors.append(loss)
 
-    def optimize(self, canvas: Canvas, reference):
+    def optimize(
+        self, canvas: Canvas, reference: NDArray, parameters: Dict[str, Parameter], brush: str
+    ):
         # create list of errors at each iteration
         self._errors = []
-        x = np.random.randint(0, canvas.resolution[1])
-        y = np.random.randint(0, canvas.resolution[0])
-        color = np.random.randint(0, 256)
-        angle = np.random.randint(0, 360)
+
         # start iterating
         for i in range(self._max_iterations):
-            while True:
-                x_old, y_old = x, y
-                x_new = x + self.sampler()
-                y_new = y + self.sampler()
-                # check if canvas bounds are not exceeded by random steps
-                if (0 <= x_new <= canvas.resolution[1]) and (0 <= y_new <= canvas.resolution[0]):
-                    x, y = x_new, y_new
-                    break
-            color_old = color
-            angle_old = angle
-            # take a random step along the color gradient
-            color = np.clip(color + np.random.randint(-50, 50, 1), 0, 255)[0]
-            angle = np.clip(angle + np.random.randint(-10, 10, 1), 0, 360)[0]
-            # calculate scale
-            scale = 200 * (2 / 200) ** (i / self._max_iterations)
-            color_tuple = (int(color),) * 3
-            canvas.draw.rectangle(center=(x, y), scale=scale, angle=angle, color=color_tuple)
+            # store parameter values before updating
+            params_previous = {k: param.value for k, param in parameters.items()}
+            # update parameters by taking a random step
+            for param in parameters.values():
+                param.update(self.sampler)
+            # get the drawing method
+            drawer = getattr(canvas.draw, brush)
+            kwargs = {k: param.value for k, param in parameters.items()}
+            # call the draw method with the kwargs
+            drawer(**kwargs)
             # calculate previous loss
             previous_loss = self.loss(canvas.previous, reference)
             # update list with previous loss value
             self.error_stats = previous_loss
-            # get standard deviation of loss
-            _, loss_std = self.error_stats
             # calculate loss after drawing
             new_loss = self.loss(canvas.canvas, reference)
-            # calculate delta in loss and normalize
-            d_e = new_loss - previous_loss  # / (loss_std + 1e-8)
+            # calculate delta in loss
+            d_e = new_loss - previous_loss
             # Evaluate the new loss against the previous loss
             if new_loss > previous_loss:
                 # calculate new acceptance criteria
-                acceptance_criterion = np.exp(-(d_e) / self.current_temp)
+                acceptance_criterion = np.exp(-d_e / self.current_temp)
                 if np.random.rand() < acceptance_criterion:
                     # Case 1: Accept new x and y but revert the canvas
                     canvas.revert()
-                    # canvas.draw.circle(center=(x, y), radius=5, color=(0, 0, 255))
-                    # Keep x and y as they are (x_new, y_new)
                 else:
                     # Case 2: Revert both the canvas and the coordinates
                     canvas.revert()
-                    # canvas.draw.circle(center=(x, y), radius=5, color=(255, 0, 0))
-                    # Revert x and y to the values before the random step
-                    x, y = x_old, y_old
-                    color = color_old
-                    angle = angle_old
-                # add new entropy criterion to list
-                self._entropy_norm.append(i)
+                    for k, param in parameters.items():
+                        param.value = params_previous[k]
             # cool down temperature
             self.current_temp *= self.cooling_rate
 
-            # stop optimization of threshold is reached
+            # stop optimization if threshold is reached
             if new_loss < self.stop_criterion:
                 print(f"Stopping early at iteration {i} with error {new_loss}")
                 break
